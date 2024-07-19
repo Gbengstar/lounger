aacc977c496d84e6f150671ca3b954c0a96247c4
Gbenga Oyewole <gbengstar@gmail.com>
2024-07-19 23:50:21 +0100

    fix: relax enviroment variable

--- config/runtime.exs
+++ config/runtime.exs
@@ -77,7 +77,7 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = env!("PHX_HOST") || "example.com"
+  host = System.get_env("PHX_HOST") || "example.com"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :lounge, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
